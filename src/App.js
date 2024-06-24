9f579e665c32f97b3d14c91a5e5968be4db0dcc4
Roland-Hufnagel <roland.hufnagel@gmx.de>
2024-06-24 15:10:29 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
 import "./styles.css";
-
+import { useState } from "react";
 export default function App() {
-  return <Counter />;
+  return (
+    <>
+      <Counter />
+      <Counter />
+      <Counter />
+    </>
+  );
 }
 
 function Counter() {
+  //   // state:
+  //   const count = 0;
+  // // setter function:
+  // function setCount(newCount){
+  //   // ... optimierungen
+  //   // count = newCount;
+  //   // Counter()
+  // }
+
+  const [count, setCount] = useState(0);
+  const [darkMode, setDarkMode] = useState(false);
+
+  function handleClick() {
+    //count = count + 1;
+    setCount(count + 1);
+    console.log("You clicked me", count);
+  }
 
   return (
-    <div className="counter">
-      <h1>You have clicked 0 times</h1>
-      <button>
-        Click me
+    <div className={darkMode ? "counter darkmode" : "counter"}>
+      <h1>You have clicked {count} times</h1>
+      <button onClick={handleClick}>Click me</button>
+      <button
+        onClick={() => {
+          setDarkMode(!darkMode);
+        }}
+      >
+        Toggle Mode
       </button>
     </div>
   );
